48f9e0018917c5b0193c40b90380ca90dfb70db4
Kiran <kiran.ramgade@succevo.com>
2024-06-24 16:43:28 +0530

    first commit

---
+++ app/src/main/java/com/kiran/compose_states/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.kiran.compose_states
+
+
+import com.kiran.compose_states.ui.theme.Compose_statesTheme
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Compose_statesTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    WellnessScreen()
+                }
+            }
+        }
+    }
+}
